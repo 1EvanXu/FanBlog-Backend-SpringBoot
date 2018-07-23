b9c4c36b97d24bbd2fbb4bb0ebc358556165a823
yifan.xu <yifan_xu6@163.com>
2018-07-23 16:42:54 +0800

    recode the generatePubId() of PubIdGenerator, now it's thread safe

--- src/main/java/com/evan/blog/util/PubIdGenerator.java
+++ src/main/java/com/evan/blog/util/PubIdGenerator.java
@@ -1,14 +1,33 @@
 package com.evan.blog.util;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.atomic.AtomicInteger;
+/*
+    还需要改进的地方：服务器停机重启之后，numberOfPub的值会丢失，等redis搭建好了之后可以将numberOfPub放入redis
+ */
 
 public class PubIdGenerator {
-    public static int generatePubId() {
 
-        Date currentTime = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyMMddS");
-        String pubIdString = simpleDateFormat.format(currentTime);
-        return Integer.parseInt(pubIdString);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyMMdd");
+
+    private static final int MAX_NUMBER_OF_PUB_EACH_DAY = 1000;
+
+    private static int oldPubIdPrefix = 0;
+
+    private static AtomicInteger numberOfPub = new AtomicInteger(0);
+
+    public static int generatePubId() {
+        int pubIdPrefix = Integer.parseInt(LocalDateTime.now().format(DATE_TIME_FORMATTER));
+        int pubId = pubIdPrefix * MAX_NUMBER_OF_PUB_EACH_DAY;
+
+        if (pubIdPrefix != oldPubIdPrefix) {
+            oldPubIdPrefix = pubIdPrefix;
+            numberOfPub.set(0);
+        }
+
+        pubId += numberOfPub.getAndIncrement();
+        return pubId;
     }
 }
+
--- src/test/java/com/evan/blog/util/PubIdGeneratorTest.java
+++ src/test/java/com/evan/blog/util/PubIdGeneratorTest.java
@@ -2,6 +2,11 @@ package com.evan.blog.util;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
 import static org.junit.Assert.*;
 
 public class PubIdGeneratorTest {
@@ -9,5 +14,35 @@ public class PubIdGeneratorTest {
     @Test
     public void generatePubId() {
         System.out.println("pubId: " + PubIdGenerator.generatePubId());
+    }
+
+    //线程安全测试
+    @Test
+    public void threadSafeGeneratePubId() {
+        int initialCapacity = 1000;
+        List<Thread> threads = new ArrayList<>(initialCapacity);
+        ConcurrentHashMap<Integer, String> resultMap = new ConcurrentHashMap<>();
+        CountDownLatch countDownLatch = new CountDownLatch(initialCapacity);
+        for (int i = 0; i < initialCapacity; i++) {
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    resultMap.put(PubIdGenerator.generatePubId(), Thread.currentThread().getName());
+                    countDownLatch.countDown();
+                }
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(1000, resultMap.size());
+
     }
 }
