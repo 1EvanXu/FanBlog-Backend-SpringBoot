06076c9b5eb5673f76c5135ffbac8ab9c6585952
yifan.xu <yifan_xu6@163.com>
2018-08-29 09:38:11 +0800

    prod test success

--- src/main/java/com/evan/blog/BlogApplication.java
+++ src/main/java/com/evan/blog/BlogApplication.java
@@ -3,10 +3,19 @@ package com.evan.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.evan.blog.repository")
-public class BlogApplication {
+@ComponentScan(basePackages = {"com.evan.blog.*"})
+public class BlogApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
