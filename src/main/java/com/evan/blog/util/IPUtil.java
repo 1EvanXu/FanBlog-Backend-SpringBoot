055c9afa47c4504bb80c0fcc2153352a05476341
yifan.xu <yifan_xu6@163.com>
2018-08-30 17:02:18 +0800

    get real ip

--- src/main/java/com/evan/blog/util/IPUtil.java
+++ src/main/java/com/evan/blog/util/IPUtil.java
@@ -69,25 +69,27 @@ public class IPUtil {
     }
 
     public static String getRealIP(HttpServletRequest request) {
-        //dev env mock different visitors with different ip
-        return getRandomIp();
 
-//        String ip = request.getHeader("X-Forwarded-For");
-//
-//        if(!(ip == null || ip.equals("")) && !"unKnown".equalsIgnoreCase(ip)){
-//            //多次反向代理后会有多个ip值，第一个ip才是真实ip
-//            int index = ip.indexOf(",");
-//            if(index != -1){
-//                return ip.substring(0,index);
-//            }else{
-//                return ip;
-//            }
-//        }
-//        ip = request.getHeader("X-Real-IP");
-//        if(!(ip == null || ip.equals("")) && !"unKnown".equalsIgnoreCase(ip)){
-//            return ip;
-//        }
-//        return request.getRemoteAddr();
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if(!(ip == null || ip.equals("")) && !"unKnown".equalsIgnoreCase(ip)){
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = ip.indexOf(",");
+            if(index != -1){
+                return ip.substring(0,index);
+            }else{
+                return ip;
+            }
+        }
+        ip = request.getHeader("X-Real-IP");
+        if(!(ip == null || ip.equals("")) && !"unKnown".equalsIgnoreCase(ip)){
+            return ip;
+        }
+        ip = request.getRemoteAddr();
+        if (ip.startsWith("192.168.") || ip.startsWith("www.") || ip == "localhost" || ip == "127.0.0.1") {
+            ip = getRandomIp();
+        }
+        return ip;
     }
 
 }
